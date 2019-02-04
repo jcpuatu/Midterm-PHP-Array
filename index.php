34b45af7b2a4260a431cf7e4dedde66330071fc9
jcpuatu <jcpuatu@gmail.com>
2019-02-04 14:15:26 +0800

    added loop

--- index.php
+++ index.php
@@ -7,7 +7,10 @@
 $fruits = array("apple"=>"10", "ponkan"=>"12", "grapes"=>"50");
 
 
-
+foreach($fruits as $x => $x_value) {
+    echo "Key=" . $x . ", Value=" . $x_value;
+    echo "<br>";
+}
 ?>
     
 </body>
